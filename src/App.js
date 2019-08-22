d49d0ac3754c73c8db7338dea20718977290ac83
Calvin Tan <hello@calvin-tan.com>
2019-08-22 18:02:58 +0800

    Add markers to the map for every flat

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 import './App.css';
 
 import Flat from './components/flat';
+import Marker from './components/marker';
 
 class App extends Component {
   constructor(props) {
@@ -22,7 +24,13 @@ class App extends Component {
       })
   }
 
+
   render() {
+    const center = {
+      lat: 48.8566,
+      lng: 2.3522
+    }
+
     return (
       <div className="app">
         <div className="main">
@@ -37,6 +45,14 @@ class App extends Component {
           </div>
         </div>
         <div className="map">
+          <GoogleMapReact
+            center={center}
+            zoom={11}
+          >
+            {this.state.flats.map((flat) => {
+              return <Marker lat={flat.lat} lng={flat.lng} text={flat.price} />
+            })}
+          </GoogleMapReact>
         </div>
       </div>
     );
