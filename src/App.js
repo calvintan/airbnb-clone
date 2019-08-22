85225baa360572cf15cda614132e14f9919af90c
Calvin Tan <hello@calvin-tan.com>
2019-08-22 17:06:03 +0800

    Display a list of flats from a json file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Flat from './components/flat';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        });
+      })
+  }
+
+  render() {
+    const flat = {
+      "id": 148,
+      "name": "Trendy Apt in Buttes Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+      "price": 200,
+      "priceCurrency": "EUR",
+      "lat": 48.885707,
+      "lng": 2.343543
+    }
+
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search">
+
+          </div>
+
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map">
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import Hello from './components/hello';
-
-ReactDOM.render(<Hello name="World" />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 
